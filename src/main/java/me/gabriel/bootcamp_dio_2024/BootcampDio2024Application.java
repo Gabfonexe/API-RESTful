2b4b87c307c6325fcb4f123656c17f2219d47ba1
Gabfonexe <bielfrancisco23@gmail.com~>
2024-07-03 19:33:51 -0300

    adicionando

---
+++ src/main/java/me/gabriel/bootcamp_dio_2024/BootcampDio2024Application.java
@@ -0,0 +1,13 @@
+package me.gabriel.bootcamp_dio_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDio2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDio2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabriel/bootcamp_dio_2024/BootcampDio2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabriel.bootcamp_dio_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDio2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
